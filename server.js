3f712cb3172a3f37f95c76fe43587ca5bc1ef4f1
unknown <jeremy.mcguirk@gmail.com>
2021-11-08 12:42:14 -0500

    populated models dir. added dependency requirements to server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+// Requiring all dependencies that will be used once program initiates 
+const path = require('path');
+const express = require('express');
+// express middleware that uses sessions, a mechanism that helps
+// applications to determine whether multi requests came in from client.
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require("./config/connection");
+
+app.use(express.static(path.join(__dirname, 'public')));
