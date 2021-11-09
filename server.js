330788dd2181f43210b454dccc91f92a4d5ba194
Jeremymcg88 <jeremy.mcguirk@gmail.com>
2021-11-08 19:40:52 -0500

    created folders to hold handlebars, routes, and updated the stylesheet to include prepopulated CSS.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Requiring all dependencies that will be used once program initiates 
 const path = require('path');
 const express = require('express');
+const routes = require('./controllers');
 // express middleware that uses sessions, a mechanism that helps
 // applications to determine whether multi requests came in from client.
 const session = require('express-session');
+// create template engine of choice 
 const exphbs = require('express-handlebars');
 
 const app = express();
