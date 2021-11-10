fd19800ee6e8b26a6c3ab6f37a1bd1c11f9e8846
unknown <jeremy.mcguirk@gmail.com>
2021-11-10 18:56:03 -0500

    installed required packages. can now NPM start and connect to seq library.

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,12 @@
 // import the Sequelize constructor from the library
 const { Sequelize } = require('sequelize');
-const sequelize = require('sequelize');
-
 // stores variables that don't need to be pushed up to server but stays local 
 require ('dotenv').config();
 
 // create connection to our db 
 const sequelize = process.env.JAWSDB_URL
-new Sequelize(process.env.JAWSDB_URL)
-new Sequelize (process.env.DB_NAME, process.db.DB_USER, process.env.DB_PW, {
+? new Sequelize(process.env.JAWSDB_URL)
+: new Sequelize (process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
