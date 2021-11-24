9656ea085695d219e0d4e65426592b80e1793ede
unknown <jeremy.mcguirk@gmail.com>
2021-11-23 23:50:23 -0500

    reduced unnecessary verification code from seed files. updated the path where the JS is coming from. Updated the server code in server.js. Can now add post, edit and delete.

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -1,106 +1,106 @@
 const { Post } = require('../models');
 
 const postdata = [
-  {
-    title: 'Fate Stay',
-    content: 'Alexander',
-    user_id: 10
-  },
-  {
-    title: 'The Devil is a Part-Timer',
-    content: 'I forgot',
-    user_id: 8
-  },
-  {
-    title: 'Tokyo Revengers',
-    content: 'Mikey',
-    user_id: 1
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 4
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 7
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 4
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 1
-  },
-  {
-    title: "",
-    content: '',
-    user_id: 1
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 9
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 5
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 3
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 10
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 8
-  },
-  {
-    title: '',
-    content: '.',
-    user_id: 3
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 3
-  },
-  {
-    title: '',
-    content: '',
-    user_id: 7
-  },
-  {
-    title: 'Naruto',
-    content: 'Guy',
-    user_id: 6
-  },
-  {
-    title: 'Jujutsu Kaisen',
-    content: 'Gojo',
-    user_id: 4
-  },
-  {
-    title: "Chainsaw Man",
-    content: 'Denji.',
-    user_id: 6
-  },
-  {
-    title: 'Demon Slayer',
-    content: 'Zenitsu',
-    user_id: 7
-  }
+  // {
+  //   title: 'Fate Stay',
+  //   content: 'Alexander',
+  //   user_id: 10
+  // },
+  // {
+  //   title: 'The Devil is a Part-Timer',
+  //   content: 'I forgot',
+  //   user_id: 8
+  // },
+  // {
+  //   title: 'Tokyo Revengers',
+  //   content: 'Mikey',
+  //   user_id: 1
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 4
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 7
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 4
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 1
+  // },
+  // {
+  //   title: "",
+  //   content: '',
+  //   user_id: 1
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 9
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 5
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 3
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 10
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 8
+  // },
+  // {
+  //   title: '',
+  //   content: '.',
+  //   user_id: 3
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 3
+  // },
+  // {
+  //   title: '',
+  //   content: '',
+  //   user_id: 7
+  // },
+  // {
+  //   title: 'Naruto',
+  //   content: 'Guy',
+  //   user_id: 6
+  // },
+  // {
+  //   title: 'Jujutsu Kaisen',
+  //   content: 'Gojo',
+  //   user_id: 4
+  // },
+  // {
+  //   title: "Chainsaw Man",
+  //   content: 'Denji.',
+  //   user_id: 6
+  // },
+  // {
+  //   title: 'Demon Slayer',
+  //   content: 'Zenitsu',
+  //   user_id: 7
+  // }
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
